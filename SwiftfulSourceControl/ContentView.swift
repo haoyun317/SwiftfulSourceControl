c74a6a87b00d44abf6c3ebe5d4b3abef9aa925a3
HaoYun <haoyun0317@gmail.com>
2024-08-02 10:35:56 +0800

    [Clean] Add commit message documentation

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -5,6 +5,24 @@
 //  Created by  謝皓昀 on 2024/8/2.
 //
 
+/*
+ COMMIT MESSAGES
+ 
+ NEW FEATURE:
+ [Feature] Description of the feature
+ 
+ BUG IN PRODUCTION:
+ [Patch] Description of patch
+ 
+ BUG NOT IN MY PRODUCTION:
+ [Bug] Description of bug
+ 
+ MUNDANE TASKS:
+ [Clean] Description of changes
+ 
+ RELEASE:
+ [Release] Description of release
+ */
 import SwiftUI
 
 struct ContentView: View {
